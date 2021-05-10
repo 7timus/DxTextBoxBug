5b65a5aefd45e65566055ba49e3658e6531dc634
Egons Kalnins <egons.kalnins@microsea.at>
2021-05-10 09:32:55 +0200

    Add project files.

---
+++ ChromWeakPasswordCheckBug/Shared/CustomAuthStateProvider.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ChromWeakPasswordCheckBug.Shared
+{
+    public interface ILogonHandler
+    {
+        Task LogonAsync();
+        Task LogoffAsync();
+    }
+
+    public class CustomAuthStateProvider : AuthenticationStateProvider, ILogonHandler
+    {
+        private AuthenticationState _authenticationState = new AuthenticationState(new ClaimsPrincipal());
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            // adding this task delay makes it work
+            //await Task.Delay(1);
+            return _authenticationState;
+        }
+
+        public async Task LogonAsync()
+        {
+            await ClearAuthStateAsync();
+
+            var identity = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Name, "mrfibuli"),
+            }, "Fake authentication type");
+
+            var user = new ClaimsPrincipal(identity);
+            _authenticationState = new AuthenticationState(user);
+
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public async Task LogoffAsync()
+        {
+            await ClearAuthStateAsync();
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        private async Task ClearAuthStateAsync()
+        {
+            _authenticationState = new AuthenticationState(new ClaimsPrincipal());
+        }
+    }
+}
